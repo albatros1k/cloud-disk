cfa25629320385be25ea8c8af6c1269c0d7e8afb
Albert <superalbertem@gmail.com>
2022-03-19 14:45:42 +0200

    JWT token authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,6 +2,8 @@ const Router = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const router = new Router();
 
@@ -25,7 +27,7 @@ router.post(
         return res.status(400).json({ message: `User with email ${email} is already exist` });
       }
 
-      const hashPassword = await bcrypt.hash(password, 15);
+      const hashPassword = await bcrypt.hash(password, 8);
 
       const user = new User({ email, password: hashPassword });
 
@@ -38,5 +40,38 @@ router.post(
     }
   }
 );
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isPassValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPassValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), { expiresIn: "1h" });
+
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ message: "Server error" });
+  }
+});
 
 module.exports = router;
