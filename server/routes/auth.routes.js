2396c1b6710e796563d80fa6d6ffe624f75f7254
Albert <superalbertem@gmail.com>
2022-03-21 16:49:48 +0200

    File services and controllers

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,8 @@ const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const authMiddleWare = require("../middleware/auth.middleware");
+const fileService = require("../services/fileService");
+const File = require("../models/File");
 
 const router = new Router();
 
@@ -33,6 +35,7 @@ router.post(
       const user = new User({ email, password: hashPassword });
 
       await user.save();
+      await fileService.createDir(new File({ user: user.id, name: "" }));
 
       return res.json({ message: "User was created" });
     } catch (error) {
---
+++ server/services/fileService.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const File = require("../models/File");
+const config = require("config");
+
+class FileService {
+  createDir(file) {
+    const filePath = `${config.get("filePath")}/${file.user}//${file.path}`;
+    return new Promise((res, rej) => {
+      try {
+        if (!fs.existsSync(filePath)) {
+          fs.mkdirSync(filePath);
+          return res({ message: "File was created" });
+        } else {
+          return rej({ message: "File already exist" });
+        }
+      } catch (error) {
+        return rej({ message: "File error" });
+      }
+    });
+  }
+}
+
+module.exports = new FileService();
